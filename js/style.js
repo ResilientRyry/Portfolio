bb19d419eb30109e018c11e8e0d4d1e158f00ae0
Ryan <r.tillman.french@gmail.com>
2019-09-01 00:24:40 -0400

    first commit

---
+++ js/style.js
@@ -0,0 +1,8 @@
+// const navbar = Array.from(document.querySelectorAll(' header ul > li'));
+// let dsc = 20;
+// for (var i = 1; i < navbar.length; i++) {
+//   navbar[i].style.margin = `10px ${dsc}\%`
+//   dsc+=20
+// }
+// console.log(navbar);
+let a = 1
